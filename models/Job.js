286a6e0f5c9850fb326c36c2a339ee5f99d3429e
Aswin <aswinash19@gmail.com>
2024-04-26 22:34:20 +0530

    Jobs Api Deploy

---
+++ models/Job.js
@@ -0,0 +1,27 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+
+const JobsSchema = new mongoose.Schema({
+    company:{
+        type:String,
+        required: [true,'Please Provide Company Name'],
+        maxlength:50,
+    },
+    position:{
+        type:String,
+        required:[true,'Please provide Position'],
+        maxlength:200,
+    },
+    status:{
+        type:String,
+        enum:['interview','declined','pending'],
+        default:'pending'
+    },
+    createdBy:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true,'Please provide User']
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model('Job',JobsSchema) 
